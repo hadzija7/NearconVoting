7dc0b466a355a7f92543460e7d825dace9099cec
hadzija7 <hadzijasale@gmail.com>
2022-09-12 19:05:01 +0100

    registerIdentity function added

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { initContract } from './utils/HelloNear';
+import { initContract } from './utils/Semaphore';
 
 const reactRoot = createRoot(document.querySelector('#root'));
 
