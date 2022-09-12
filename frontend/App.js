46d6f70037dd01bfd6415a562902996d16bda688
hadzija7 <hadzijasale@gmail.com>
2022-09-12 19:25:24 +0100

    registering identity finished

--- frontend/App.js
+++ frontend/App.js
@@ -1,24 +1,52 @@
 import 'regenerator-runtime/runtime';
+//react imports
 import React from 'react';
-//react router
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 //import components
 import HelloNear from './components/HelloNear';
-// import Polls from './components/Polls';
 import Poll from './components/Polls/Poll';
-// import About from './components/About';
+//semaphore imports
+import { Identity } from "@semaphore-protocol/identity"
+//util imports
+import { registerIdentity as registerID } from './utils/Semaphore';
 
 export default function App() {
+  const [registered, setRegistered] = useState(false)
+
+  const registerIdentity = async () => {
+    console.log("Registering...")
+    //generate identity commitment
+    const identity = new Identity()
+    const commitment = identity.generateCommitment()
+    //call the utils method
+    await registerID(commitment.toString())
+    //store the identity in the local storage
+    localStorage.setItem('idCommitment', JSON.stringify(commitment))
+    //set the state variable
+    setRegistered(true)
+  }
+
+  const renderRegisterButton = () => {
+    let result
+    if(registered){
+      result = <label>Registered</label>
+    }else{
+      result = <button onClick={registerIdentity}>Register</button>
+    }
+    return result
+  }
+
   return (
     <Router>
         <div>
             <h2>Voting app</h2>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-                <li><Link to={'/'} className="nav-link"> Home </Link></li>
-                {/* <li><Link to={'/about'} className="nav-link">About</Link></li> */}
-                <li><Link to={'/polls'} className="nav-link">Polls</Link></li>
-            </ul>
+              <div className="navbar-nav mr-auto">
+                <div><Link to={'/'} className="nav-link"> Home </Link></div>
+                <div><Link to={'/polls'} className="nav-link">Polls</Link></div>
+                <div>{renderRegisterButton()}</div>
+              </div>
             </nav>
             <hr />
             <Switch>
